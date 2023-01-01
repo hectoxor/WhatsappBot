4c688f1a4b96d45ed23483405950c33c07cabe6a
hectoxor <105129772+hectoxor@users.noreply.github.com>
2023-01-02 04:31:35 +0600

    Update 0000.py

--- 0000.py
+++ 0000.py
@@ -5,7 +5,7 @@ def send_message():
    message = input('msg')
    hour = int(input('h'))
    minutes = int(input('m'))
-   #pywhatkit.sendwhatmsg(phone_no = mobile, message = message,time_hour = hour, time_min = minutes)
+   #pywhatkit.sendwhatmsg(phone_no = mobile, message = message,time_hour = hour, time_min = minutes) - to a single person
    pywhatkit.sendwhatmsg_to_group("group_id", message, hour, minutes)
 
 def main():
